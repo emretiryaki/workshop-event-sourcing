c16afdafa40cb203cec17703eb963063decab35f
Alper Hankendi <ahankendi@google.com>
2018-11-11 19:36:20 +0300

    adding exmaple before start coding...

---
+++ Reviews.Service.WebApi/Modules/Reviews/Projections/ActiveReviews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Reviews.Core.Projections;
+
+namespace Reviews.Service.WebApi.Modules.Reviews.Projections
+{
+    public class ActiveReviews : Projection
+    {
+        public override Task Handle(object e)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        
+        private static string DocumentId(Guid id) => $"ActiveReviews/{id}";
+    }
+}
